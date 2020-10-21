55bf16a52950d02971300accb376692ca6dac780
mogggen <57505931+mogggen@users.noreply.github.com>
2020-10-21 10:27:28 +0200

    Commit #1

---
+++ InstantProjectBuilder.py
@@ -0,0 +1,20 @@
+import os
+#make sure Cmake is in PATH
+proj = input("project name: ")
+
+# main.cpp
+f = open(proj + ".cpp", "w")
+f.write("int main()\n{\n\t\n}")
+f.close()
+
+# CmakeLists.txt
+f = open("CMakeLists.txt", "w")
+f.write("cmake_minimum_required(VERSION 3.6)\n" + 
+"project("+proj+")\n" +
+"add_executable(app " + proj + ".cpp)\n"+
+"set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY VS_STARTUP_PROJECT app)")
+f.close()
+
+os.system("cmake -S . -B ./build/")
+path = "\""+os.getcwd().replace("\\", "/")+"/build/"+proj+".sln\""
+os.system(path)
