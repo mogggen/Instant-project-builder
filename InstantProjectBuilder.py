1eb164222bc0ee4d068effb8a16a5fe0e3237388
mogggen <57505931+mogggen@users.noreply.github.com>
2020-10-26 14:32:05 +0100

    disapear

--- InstantProjectBuilder.py
+++ InstantProjectBuilder.py
@@ -3,11 +3,11 @@ import os
 proj = input("project name: ")
 
 # main.cpp
-f = open(proj + ".cpp", "w")
+f = open("\"" + proj + ".cpp\"", "w")
 f.write("int main()\n{\n\t\n}")
 f.close()
 
-# CmakeLists.txt
+# Make "CmakeLists.txt"
 f = open("CMakeLists.txt", "w")
 f.write("cmake_minimum_required(VERSION 3.6)\n" + 
 "project("+proj+")\n" +
@@ -15,6 +15,7 @@ f.write("cmake_minimum_required(VERSION 3.6)\n" +
 "set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY VS_STARTUP_PROJECT app)")
 f.close()
 
+#open .sln
 os.system("cmake -S . -B ./build/")
-path = "\""+os.getcwd().replace("\\", "/")+"/build/"+proj+".sln\""
+path = "\""+os.getcwd()+"\\build\\"+proj+".sln\""
 os.system(path)
