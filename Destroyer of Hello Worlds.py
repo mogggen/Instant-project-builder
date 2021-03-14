4e417cac7fc53fe8a839b8e17c4c418672668db6
Morgan Nyman <57505931+mogggen@users.noreply.github.com>
2021-03-14 21:21:38 +0100

    Delete Destroyer of Hello Worlds.py

--- Destroyer of Hello Worlds.py
+++
@@ -1,9 +0,0 @@
-import os
-s = "!}{?"
-while(1):
-    input(os.getcwd() + s)
-    command  = "echo \"" + s + "\"" + " >> " + "\"FFFF FFFF FFFF FFFF\""
-    print(command)
-    os.system(command)
-    input("are you sure about that?")
-    
