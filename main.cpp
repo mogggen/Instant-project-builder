2a86994eb05d441e1aaba92ee7beb638f17194f8
Morgan Nyman <mornym-9@student.ltu.se>
2021-09-06 16:20:13 +0200

    thread id remaing and bash

--- main.cpp
+++ main.cpp
@@ -10,6 +10,13 @@
 #include <sys/sysinfo.h>
 #include <sys/utsname.h>
 #include <pthread.h>
+#include <thread>
+#include <cmath>
+
+long long
+add_to_running_sum()
+{
+}
 
 int
 main(int argc, char** argv)
@@ -27,7 +34,7 @@ main(int argc, char** argv)
             if (gethostname(hostname, 256))
             {
                 std::cerr << "Cannot find hostname";
-                throw 0;
+                return 1;
             }
             std::cout << "hostname: " << hostname << std::endl;
 
@@ -50,40 +57,61 @@ main(int argc, char** argv)
             {
                 std::cout << "Unable to open file";
             }
-            //proc/meminfo
+            continue; // becuase I don't trust myself :D
         }
 
-        if (!strcmp(argv[i], "-F"))
+        // running sum
+        if (i + 1 >= argc)
         {
-            // running sum
-            if (true)//std::__is_integer<char*>(argv[i + 1]))
-            {
-                int X = std::stoi(argv[i + 1]);
-                for (size_t i = 0; i < X; i++)
-                {
-                    //Threads summing
-                    fork();
-                }
-                
-            }
-            else
-            {
-                std::cerr << "X was not an integer! Exiting..." << std::endl;
-            }
-            }
-            else
-            {
-                std::cerr << "X was not given! Exiting..." << std::endl;
-                return 1;
-            }
+            std::cerr << "X was not given! Exiting..." << std::endl;
+            return 1;
+        }
+        int X;
+        try
+        {
+            X = std::stoi(argv[i + 1]);
+        }
+        catch (std::exception& e)
+        {
+            printf("%s() raised a type exception: argument \"%s\" could not be parsed to a integer!\n", e.what(), argv[i + 1]);
+            return 1;
         }
         
-        if (!strcmp(argv[i], "-t"))
+        uint running_sum = 0;
+        for (size_t i = 0; i < X; i++)
         {
-            //system("top");
-
+            int p0;
+            if (!strcmp(argv[i], "-f"))
+            {
+                fork();
+                if (p0)
+                {
+                    printf("thread-process-id %5i\n", p0);
+                }
+            }
+            else if (!strcmp(argv[i], "-t"))
+            {
+                std::thread worker(add_to_running_sum);
+                std::cout << ((std::thread::id*)&(worker.get_id())) << std::endl;
+                worker.detach();
+                if (p0)
+                {
+                    printf("fork-process-id %5i\n", p0);
+                }
+            }
+            running_sum += p0;
         }
-    }
 
+        // loop and sum
+        double loop_sum = 0;
+        for (size_t i = 1; i < 50000; i++)
+        {
+            for (size_t j = 1; j < i; j++)
+            {
+                loop_sum += sqrt(i * j);
+            }
+        }
+        printf("the resulting sum is %i", loop_sum);
+    }
     return 0;
-}
+} // int main
